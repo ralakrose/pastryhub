12d596f2ec5cffbe93a7e6218845173e7a371134
rose ralak <ralakrose@gmail.com>
2021-10-30 17:20:52 +0300

    item specs

--- products.js
+++ products.js
@@ -1,15 +1,10 @@
-let wrapper = document.querySelector('.wrapper')
-let productname = document.getElementsByTagName('h1')
-let price = document.getElementsByClassName('price')
-let buybutton = document.querySelector('.product-price-button')
-let buybtn = document.querySelector('#btn')
-let productinfo = document.querySelector('.product-info')
+let wrapper = document.querySelector('.wrapper');
+let productname = document.getElementsByTagName('h1');
+let price = document.getElementsByClassName('price');
+let buybutton = document.querySelector('.product-price-button');
+let buybtn = document.querySelectorAll('#btn');
+let productinfo = document.querySelector('.product-info');
 
 //Adding item to cart
-function addToCart() 
-{
-    if (buybtn.innerHTML=="ADD TO CART") buybtn.innerHTML = "REMOVE FROM CART";
-    else buybtn.innerHTML = "ADD TO CART";
-   
-}
+
 
